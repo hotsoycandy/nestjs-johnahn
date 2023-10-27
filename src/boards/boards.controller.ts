d9e72f0cc1b44c57d481520bc07fff9b40421b95
hotsoycandy <wkdwnsghd617@gmail.com>
2023-10-27 17:12:50 +0900

    Feat: [board-status-validation.pipe] new pipe

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -12,6 +12,7 @@ import {
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDTO } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -36,7 +37,7 @@ export class BoardsController {
   @Patch('/:boardId/status')
   updateBoardStatus(
     @Param('boardId') boardId: string,
-    @Body('status') status: BoardStatus,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
   ): Board {
     return this.boardsService.updateBoardStatus(boardId, status);
   }
---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,23 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    console.log(value);
+    console.log(metadata);
+
+    value = value.toUpperCase();
+
+    if (!this.StatusOptions.includes(value)) {
+      throw new BadRequestException(`${value} isn't in the status options`);
+    }
+
+    return value;
+  }
+}
