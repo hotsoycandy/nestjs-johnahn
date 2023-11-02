2b00c16f61f74ac48666ce283a711d0b7f936ab6
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-02 23:52:21 +0900

    Feat: [auth.module] Inject user.repository

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+import { DatabaseModule } from 'src/database.module';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UserRepository } from './user.repository';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [UserRepository, AuthService],
 })
 export class AuthModule {}
