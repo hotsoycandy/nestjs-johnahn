1fc6671d9e4006c00cfbd2b36acb91c461e32e99
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-03 00:05:26 +0900

    Feat: [user.repository] Add method createUser

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialDTO } from './dto/auth-credential.dto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -10,5 +11,12 @@ export class UserRepository extends Repository<User> {
   ) {
     const repository = dataSource.getRepository(User);
     super(repository.target, repository.manager, repository.queryRunner);
+  }
+
+  async createUser(authCredentialDTO: AuthCredentialDTO): Promise<User> {
+    const { username, password } = authCredentialDTO;
+    const user = this.create({ username, password });
+    await this.save(user);
+    return user;
   }
 }
