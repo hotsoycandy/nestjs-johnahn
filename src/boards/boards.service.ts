0d08374fe818047ecf557d7e3f3b24fb84a9152b
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-02 14:10:56 +0900

    Feat: [board.service] Add method getBoardById

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,7 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Board } from './board.entity';
 import { BoardRepository } from './board.repository';
 
 @Injectable()
 export class BoardsService {
   constructor(private boardRepository: BoardRepository) {}
+
+  async getBoardById(id: number): Promise<Board> {
+    const board = await this.boardRepository.findOne(id);
+    if (!board) {
+      throw new NotFoundException(`Can't find board with id ${id}`);
+    }
+    return board;
+  }
 }
