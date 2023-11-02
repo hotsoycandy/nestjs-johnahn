6d2f38c6ee376d64445f1da180daa3385a38ae24
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-03 00:10:50 +0900

    Feat: [auth.controller] Add method signUp

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthCredentialDTO } from './dto/auth-credential.dto';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(@Body() authCredentialDTO: AuthCredentialDTO): Promise<User> {
+    return this.authService.signUp(authCredentialDTO);
+  }
 }
