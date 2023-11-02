558db24e2baaa20e197d9c980d5d25e6c3d0751c
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-02 16:32:53 +0900

    Feat: [board.controller] Add method createBoard

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Param, Get } from '@nestjs/common';
+import { Controller, Param, Get, Post, Body } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.entity';
+import { CreateBoardDTO } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
+
+  @Post('/')
+  createBoard(@Body() createBoardDTO: CreateBoardDTO): Promise<Board> {
+    return this.boardsService.createBoard(createBoardDTO);
+  }
 
   @Get('/:boardId')
   getBoardById(@Param('boardId') boardId: number): Promise<Board> {
