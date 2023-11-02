9ce589ac7c084b8ecc5a696fb81bd4cbf612a42d
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-03 00:05:42 +0900

    Feat: [auth.service] Add method signUp

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
 import { UserRepository } from './user.repository';
+import { AuthCredentialDTO } from './dto/auth-credential.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private userRepository: UserRepository) {}
+
+  async signUp(authCredentialDTO: AuthCredentialDTO): Promise<User> {
+    return this.userRepository.createUser(authCredentialDTO);
+  }
 }
