d3029028c331b584e886d257dec27d2e3e411288
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-02 16:32:33 +0900

    Feat: [board.service] Add method createBoard

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,15 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board } from './board.entity';
+import { CreateBoardDTO } from './dto/create-board.dto';
 import { BoardRepository } from './board.repository';
 
 @Injectable()
 export class BoardsService {
   constructor(private boardRepository: BoardRepository) {}
+
+  async createBoard(createBoardDTO: CreateBoardDTO): Promise<Board> {
+    return await this.boardRepository.createBoard(createBoardDTO);
+  }
 
   async getBoardById(id: number): Promise<Board> {
     const board = await this.boardRepository.findOne({ where: { id } });
