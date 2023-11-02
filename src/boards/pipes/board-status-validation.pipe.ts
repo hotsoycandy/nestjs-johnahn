6ab54ad1e6128cefe1d80daa18e830b3913c8ac6
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-02 21:07:39 +0900

    Chore: [board-status-validation.pipe] Delete logs
    delete unused logs

--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -1,17 +1,10 @@
-import {
-  PipeTransform,
-  ArgumentMetadata,
-  BadRequestException,
-} from '@nestjs/common';
+import { PipeTransform, BadRequestException } from '@nestjs/common';
 import { BoardStatus } from '../types/board-status.enum';
 
 export class BoardStatusValidationPipe implements PipeTransform {
   readonly StatusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
 
-  transform(value: any, metadata: ArgumentMetadata) {
-    console.log(value);
-    console.log(metadata);
-
+  transform(value: any) {
     value = value.toUpperCase();
 
     if (!this.StatusOptions.includes(value)) {
