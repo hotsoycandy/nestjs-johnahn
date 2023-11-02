8f93d94eef3cc76fb80c46efb616a371022e7f15
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-02 14:09:24 +0900

    Feat: [board.service] Remove all method

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,44 +1,7 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { v1 as uuid } from 'uuid';
-import { Board, BoardStatus } from './board.model';
-import { CreateBoardDTO } from './dto/create-board.dto';
+import { Injectable } from '@nestjs/common';
+import { BoardRepository } from './board.repository';
 
 @Injectable()
 export class BoardsService {
-  private boards: Board[] = [];
-
-  getBoards(): Board[] {
-    return this.boards;
-  }
-
-  createBoard(createBoardDTO: CreateBoardDTO): Board {
-    const { title, description } = createBoardDTO;
-    const board: Board = {
-      id: uuid(),
-      title,
-      description,
-      status: BoardStatus.PUBLIC,
-    };
-    this.boards.push(board);
-    return board;
-  }
-
-  getBoardById(id: string): Board {
-    const board = this.boards.find((board) => board.id === id);
-    if (board === undefined) {
-      throw new NotFoundException("Can't find board with id");
-    }
-    return board;
-  }
-
-  updateBoardStatus(id: string, status: BoardStatus): Board {
-    const board = this.getBoardById(id);
-    board.status = status;
-    return board;
-  }
-
-  deleteBoard(id: string): void {
-    this.getBoardById(id);
-    this.boards = this.boards.filter((board) => board.id !== id);
-  }
+  constructor(private boardRepository: BoardRepository) {}
 }
