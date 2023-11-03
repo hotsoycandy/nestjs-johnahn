152f47f55e0b99d33350f1cfe2386f8d9273f811
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-03 15:41:58 +0900

    Feat: [auth.service] Add method signIn

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import * as crypto from 'crypto';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDTO } from './dto/auth-credential.dto';
@@ -9,5 +10,21 @@ export class AuthService {
 
   async signUp(authCredentialDTO: AuthCredentialDTO): Promise<User> {
     return this.userRepository.createUser(authCredentialDTO);
+  }
+
+  async signIn(authCredentialDTO: AuthCredentialDTO): Promise<User> {
+    const { username, password } = authCredentialDTO;
+
+    const hashedPassword = crypto
+      .createHash('sha512')
+      .update(password + 'iamsalt')
+      .digest('base64');
+
+    const user = await this.userRepository.findOne({ where: { username } });
+    if (user && user.password === hashedPassword) {
+      return user;
+    }
+
+    throw new UnauthorizedException('login failed');
   }
 }
