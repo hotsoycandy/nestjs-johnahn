4f020df76b1823e65d0ae8685149488afde7fbbc
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-03 13:17:19 +0900

    Feat: [auth-credential.dto] Add more validations

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,9 +1,16 @@
-import { IsString } from 'class-validator';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class AuthCredentialDTO {
   @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
 
   @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]{2,20}$/, {
+    message: 'password only accepts alphabets and numbers',
+  })
   password: string;
 }
