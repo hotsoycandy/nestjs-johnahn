1c0ee981e87953b89b01fbb1004e2d7d5479906d
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-03 17:15:02 +0900

    Feat: [boards.module] Import AuthModule

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database.module';
+import { AuthModule } from 'src/auth/auth.module';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { BoardRepository } from './board.repository';
-import { DatabaseModule } from 'src/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [BoardsController],
   providers: [BoardRepository, BoardsService],
 })
